d7afd4f0e1fa1897af74f4a98c1f6a5b4c6682cf
Vinicius Guerra <vini.fms@hotmail.com>
2024-04-16 14:07:01 -0300

    Validate Token middleware

---
+++ src/middlewares/validateToken.middleware.ts
@@ -0,0 +1,19 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/appError";
+import jwt from "jsonwebtoken";
+
+export class ValidateToken {
+    static execute(req: Request, res: Response, next: NextFunction){
+        const token = req.headers.authorization;
+
+        if(!token){
+            throw new AppError(403, "Token is required");
+        }
+
+        jwt.verify(token, process.env.JWT_SECRET as string);
+
+        res.locals.decode = jwt.decode(token);
+
+        next();
+    }
+}
